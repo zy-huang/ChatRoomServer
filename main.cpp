cc3c01af431c672d13fc0dc3d48ff341febf45e3
zyhuang <zone.zyhuang@gmail.com>
2016-10-07 21:13:41 +0800

    添加回射字符串的操作

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 #include<sys/socket.h>
 #include<netinet/in.h>
 #include<netinet/ip.h>
+#include<iostream>
+#include<cstring>
+#include<unistd.h>
+using namespace std;
 int main(int argc, char *argv[])
 {
     int servfd = Socket(AF_INET,SOCK_STREAM,0);
@@ -9,8 +13,13 @@ int main(int argc, char *argv[])
     unsigned int cl_addrlen;
     struct sockaddr_in addr;
     struct sockaddr client_addr;
+    char recv[100];
 
     Bind(servfd,GetSockaddr(&addr,9999,"127.0.0.1"),sizeof(struct sockaddr));
     Listen(servfd,5);
     clientfd = Accept(servfd,&client_addr,&cl_addrlen);
+    cout<<"accept"<<endl;
+
+    read(clientfd,recv,sizeof(recv));
+    write(clientfd,recv,strlen(recv));
 }
